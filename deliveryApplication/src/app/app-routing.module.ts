fc5ddb22eab38c37757ec45ae5f81967239e260b
yashas2903 <yashasaskme@gmail.com>
2022-11-14 18:43:09 -0600

    created a login component

--- deliveryApplication/src/app/app-routing.module.ts
+++ deliveryApplication/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent},
+  { path: 'login', component : LoginComponent}
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const  routingComponents = [HomePageComponent,LoginComponent];
--- deliveryApplication/src/app/app.module.ts
+++ deliveryApplication/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -18,3 +20,4 @@ import { HomePageComponent } from './home-page/home-page.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+//wujqdlefrxmhnwfqcs@tmmcv.net
