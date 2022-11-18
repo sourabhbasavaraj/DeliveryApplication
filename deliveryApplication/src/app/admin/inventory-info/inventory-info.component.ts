68f7abe9225f727623544513b78e2244405f23ed
Ashish4n8 <ashishnagaraj123@gmail.com>
2022-11-17 19:55:54 -0600

    ashish's commit

---
+++ deliveryApplication/src/app/admin/inventory-info/inventory-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-info',
+  templateUrl: './inventory-info.component.html',
+  styleUrls: ['./inventory-info.component.css']
+})
+export class InventoryInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- deliveryApplication/src/app/app.module.ts
+++ deliveryApplication/src/app/app.module.ts
@@ -6,13 +6,29 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
+import { CouponComponent } from './admin/coupon/coupon.component';
+import { WarehouseInfoComponent } from './admin/warehouse-info/warehouse-info.component';
+import { InventoryInfoComponent } from './admin/inventory-info/inventory-info.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { PriceComponent } from './price/price.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    AdminComponent,
+    CouponComponent,
+    WarehouseInfoComponent,
+    InventoryInfoComponent,
+    AboutComponent,
+    ServicesComponent,
+    PriceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
